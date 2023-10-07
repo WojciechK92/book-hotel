44eddf098848b53d3890c408497f9a0ac8f96abe
“WojciechK92” <wojciechkwitek92@gmail.com>
2023-10-07 18:12:25 +0200

    First stylization (pagination, filtration and trips)

--- app/controllers/trip-controller.js
+++ app/controllers/trip-controller.js
@@ -1,6 +1,28 @@
 class TripController {
   showAllInclusive(req, res) {
-    res.render('pages/trips/trips');
+    const food = [
+      {value: 'all-inclusive', label: 'All inclusive', checked: true}, 
+      {value: 'HB', label: 'Breakfast and dinner', checked: false}, 
+      {value: 'BB', label: 'Breakfast', checked: false}, 
+      {value: 'FB', label: 'Three meals', checked: false}, 
+    ];
+
+    const popular = [
+      {value: 'last-minute', label: 'Last minute', checked: false}, 
+      {value: 'winter2023', label: 'Winter 2023', checked: false}, 
+      {value: 'summer2024', label: 'Summer 2024', checked: false}, 
+      {value: 'poland2024', label: 'Poland 2024', checked: true}, 
+    ];
+    
+    const trips = [
+      1, 2, 3
+    ];
+    
+    res.render('pages/trips/trips', {
+      food,
+      popular,
+      trips, 
+    });
   };
 };
 
